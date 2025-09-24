f67558ea0ab43943e9b7a9f16eb604ad6ccd06d4
Cleberson Gomes <cleberson.gomess@outlook.com>
2025-09-24 08:39:14 -0300

    Remove All Files

--- src/main/java/com/gestao/projetos/repository/ProjetoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestao.projetos.repository;
-
-import com.gestao.projetos.model.Projeto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
-}
